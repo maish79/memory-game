24a951d166b41fce32588b934e472aa9c2203a25
maish79 <maishsupa@yahoo.com>
2023-03-01 20:57:44 +0000

    Created variable allClicks to increase the count by 1  when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("maish");
+var allAnimals = ["bird", "deer", "dog", "fish", "frog", "lion", "rabbit", "tiger","bird", "deer", "dog", "fish", "frog", "lion", "rabbit", "tiger"];
+var matchedPair = 0;
+var clickedAnimals = [];
+
+function displayAnimal(aid){
+    var allMoves = document.getElementById("moves").innerHTML;
+    allMoves++;
+    document.getElementById("moves").innerHTML = allMoves;
+    
+}
