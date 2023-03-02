0cac224044801f7b4107ab264cac461c92fcd3ae
maish79 <maishsupa@yahoo.com>
2023-03-02 19:40:55 +0000

    Added alert pop-up when the game is finished or by getting matched pair

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,22 +18,29 @@ function displayAnimal(aid)
         if(clickedAnimals.length ==2)
         {  
              if(document.getElementById(clickedAnimals[0]).getAttribute("src") == document.getElementById(clickedAnimals[1]).getAttribute("src"))
-             {
+            { 
                 matchedPair++;
+               
                 setTimeout(function(){
                     clickedAnimals = [];
+                   alert('you have a match');
                 }, 500);
-                if(matchedPair == 8)
-                document.getElementById("restartScreen").style.display = "block";
-            }
-            else
-            {
+               
+                if(matchedPair == 8){
+                    setTimeout(function(){
+                    alert('Congradulations, you win');
+                    document.getElementById("restartScreen").style.display = "block";
+                }, 500);
+                }
+            }else{
+            
                 setTimeout(function(){
                     document.getElementById(clickedAnimals[0]).style.display = "none";
                     document.getElementById(clickedAnimals[1]).style.display = "none";
                     document.getElementById("animal"+clickedAnimals[0]).style.pointerEvents = "auto";
                     document.getElementById("animal"+clickedAnimals[1]).style.pointerEvents = "auto";
                     clickedAnimals= [];
+                   //alert('sorry, try again!');
                 },500);
             }
         }
