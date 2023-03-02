d7d44e9d6b7f658f926c094b085004eba3cefb8d
maish79 <maishsupa@yahoo.com>
2023-03-02 11:05:26 +0000

    Added clickedAnimals loop to push the animals id into the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,13 @@ function displayAnimal(aid){
     var allMoves = document.getElementById("moves").innerHTML;
     allMoves++;
     document.getElementById("moves").innerHTML = allMoves;
+
+    if(clickedAnimals.length !=2){
+        document.getElementById(aid).style.display = "block";
+        var animal = document.getElementById(aid).src.split("/").pop().split(".")[0];
+        document.getElementById("animal"+aid).style.pointerEvents= "none";
+        clickedAnimals.push(aid);
+        
+    }
     
 }
