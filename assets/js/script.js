2bf4bb2ba941de0b9d7d4b70d294f7f3add50b9b
maish79 <maishsupa@yahoo.com>
2023-03-02 18:21:43 +0000

    created media queries for all devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,41 @@ var allAnimals = ["bird", "deer", "dog", "fish", "frog", "lion", "rabbit", "tige
 var matchedPair = 0;
 var clickedAnimals = [];
 
-function displayAnimal(aid){
+function displayAnimal(aid)
+{   
     var allMoves = document.getElementById("moves").innerHTML;
     allMoves++;
     document.getElementById("moves").innerHTML = allMoves;
 
-    if(clickedAnimals.length !=2){
+    if(clickedAnimals.length !=2)
+    {
         document.getElementById(aid).style.display = "block";
         var animal = document.getElementById(aid).src.split("/").pop().split(".")[0];
         document.getElementById("animal"+aid).style.pointerEvents= "none";
         clickedAnimals.push(aid);
-
-        if(clickedAnimals.length ==2){
-            if(document.getElementById(clickedAnimals[0]).getAttribute("src") == document.getElementById(clickedAnimals[1]).getAttribute("src")){
-            matchedPair++;
-            setTimeout(function(){
-                clickedAnimals = [];
-            }, 500);
-            if(matchedPair == 8)
-            document.getElementById("restartScreen").style.display = "block";
-
+    
+        if(clickedAnimals.length ==2)
+        {  
+             if(document.getElementById(clickedAnimals[0]).getAttribute("src") == document.getElementById(clickedAnimals[1]).getAttribute("src"))
+             {
+                matchedPair++;
+                setTimeout(function(){
+                    clickedAnimals = [];
+                }, 500);
+                if(matchedPair == 8)
+                document.getElementById("restartScreen").style.display = "block";
+            }
+            else
+            {
+                setTimeout(function(){
+                    document.getElementById(clickedAnimals[0]).style.display = "none";
+                    document.getElementById(clickedAnimals[1]).style.display = "none";
+                    document.getElementById("animal"+clickedAnimals[0]).style.pointerEvents = "auto";
+                    document.getElementById("animal"+clickedAnimals[1]).style.pointerEvents = "auto";
+                    clickedAnimals= [];
+                },500);
+            }
         }
-        else{
-            setTimeout(function(){
-                document.getElementById(clickedAnimals[0]).style.display = "none";
-                document.getElementById(clickedAnimals[1]).style.display = "none";
-                document.getElementById("animal"+clickedAnimals[0]).style.pointerEvents = "auto";
-                document.getElementById("animal"+clickedAnimals[1]).style.pointerEvents = "auto";
-                clickedAnimals= [];
-            },500);
-        }
-    }
 
     }
     
